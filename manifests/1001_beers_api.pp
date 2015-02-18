b3cfd0c614ad8f02bdb6f3454c009bc53ea44f15
Iszak Bryan <iszak.bryan@gmail.com>
2015-02-18 22:32:13 +0000

    Doesn't require lib magic

--- manifests/1001_beers_api.pp
+++ manifests/1001_beers_api.pp
@@ -22,7 +22,6 @@ class role::1001_beers_api (
 
     project::rails { '1001_beers_api':
         require           => [
-            Package['libmagic-dev'],
             Package['libsqlite3-dev']
         ],
 
