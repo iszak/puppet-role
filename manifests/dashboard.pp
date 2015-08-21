b85e8608218a86943595def5ec7db4883c3203c6
Iszak Bryan <iszak.bryan@gmail.com>
2015-08-22 00:07:11 +0100

    Added monitor agent to dashboard

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,3 +1,15 @@
-class role::dashboard () {
-  include ::profile::monitor::backend
+class role::dashboard (
+  $monitor,
+  $monitor_backend = undef,
+) {
+  validate_bool($monitor)
+  validate_string($monitor_backend)
+
+  if $monitor {
+    class { '::profile::monitor::agent':
+      backend => $monitor_backend,
+    }
+  }
+
+  class { '::profile::monitor::backend': }
 }
